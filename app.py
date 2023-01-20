fefb09737908f53d63723bc88fc948e93eb49c3a
raerae1205 <crashdean@hotmail.com>
2023-01-19 22:05:11 -0800

    Add screen shots

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_craigslist
+from flask import Flask, render_template, redirect, request
+
+
 
 app = Flask(__name__)
 
@@ -14,17 +14,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    
+    return render_template("index.html")
 
 
-@app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update_one({}, {"$set": listings_data}, upsert=True)
-    return redirect("/", code=302)
-
-
+@app.route("/form")
+def form():
+     if request.method == "POST":
+        print ("hello")
 if __name__ == "__main__":
     app.run(debug=True)
