6a582b0ee26b8747bd07c430698c1b9c0c2d48c1
raerae1205 <crashdean@hotmail.com>
2023-01-25 18:58:50 -0800

    create html model

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template, redirect, request
-
-
-
+import pickle
+import joblib
+import pandas as pd
+import numpy as np
+from tensorflow import keras
 app = Flask(__name__)
-
-# Use flask_pymongo to set up mongo connection
-#app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-#mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
 
 @app.route("/")
 def index():
@@ -18,9 +12,33 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/form")
+@app.route("/form", methods = ['POST'])
 def form():
-     if request.method == "POST":
-        print ("hello")
+     
+    print("hello")
+    input_list = []
+    input_list.append(float(request.form['age']))
+    input_list.append(float(request.form['gender']))
+    input_list.append(float(request.form['chest_pain']))
+    input_list.append(float(request.form['trestbps']))
+    input_list.append(float(request.form['chol']))
+    input_list.append(float(request.form['fbs']))
+    input_list.append(float(request.form['restecg']))
+    input_list.append(float(request.form['thalach']))
+    input_list.append(float(request.form['exang']))
+    input_list.append(float(request.form['oldpeak']))
+    input_list.append(float(request.form['location']))
+    print(input_list)
+    input_list=[input_list]
+    scaler = joblib.load('notebooks/scaler.save')
+    pickled_model = pickle.load(open('notebooks/nn_model.pkl', 'rb'))
+    tem_df=pd.DataFrame(input_list)
+    xinput = scaler.transform(tem_df)
+    print(xinput)
+    #finished=pickled_model.predict(xinput)
+    reconstructed_model = keras.models.load_model("notebooks/nn") 
+    finished=reconstructed_model.predict(xinput)
+    print(finished)
+    return render_template("index.html",prediction_result=finished[0][0])
 if __name__ == "__main__":
     app.run(debug=True)
